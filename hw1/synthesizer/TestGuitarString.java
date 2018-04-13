4cf06219d2f526262682acbe64f4b3239f8ef15e
Shinny <ms.shinny.hoo@gmail.com>
2018-04-13 15:08:35 +0800

    Add the StdAudiio.java to handle sound.

--- hw1/synthesizer/TestGuitarString.java
+++ hw1/synthesizer/TestGuitarString.java
@@ -1,14 +1,28 @@
 package synthesizer;
 
-
 public class TestGuitarString {
 
-    /** Calls tests for GuitarString. */
-    public static void main(String[] args) {
+	// Plays A- note:
+	public static void testPluckTheAString() {
         double CONCERT_A = 440.0;
         GuitarString aString = new GuitarString(CONCERT_A);
         aString.pluck();
+        for (int i = 0; i < 50000; i += 1) {
+            StdAudio.play(aString.sample());
+            aString.tic();
+        }
+    }
+
+    public static void testTteration() {
+    	double CONCERT_A = 440.0;
+        GuitarString aString = new GuitarString(CONCERT_A);
+        aString.pluck();
         aString.iteratePrint();
+    }
+
+    /** Calls tests for GuitarString. */
+    public static void main(String[] args) {
+    	testPluckTheAString();
 
     }
 } 
