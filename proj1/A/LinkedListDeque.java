0592fbf9d64d1a5395565e5d93536e9fd61f2aec
Shinny <ms.shinny.hoo@gmail.com>
2018-04-13 15:30:19 +0800

    Seperate the test method in another class.

--- proj1/A/LinkedListDeque.java
+++ proj1/A/LinkedListDeque.java
@@ -101,42 +101,4 @@ public class LinkedListDeque<T> {
         return getRecursive(index--);
     }
 
-    public static void main(String[] args) {
-
-        // Some testing:
-        LinkedListDeque<Integer> list = new LinkedListDeque<Integer>();
-        list.addFirst(1);
-        list.addFirst(2);
-        list.addFirst(3);
-        list.addFirst(4);
-        list.addLast(0);
-        list.addLast(-1);
-        list.printDeque();
-        list.removeFirst();
-        list.removeFirst();
-        list.removeLast();
-        list.printDeque();
-        System.out.println((1 + 5) % 5);
-
-        ArrayDeque<Integer> arr = new ArrayDeque<Integer>();
-        arr.addFirst(1);
-        arr.addFirst(2);
-        arr.addFirst(3);
-        arr.addFirst(4);
-        arr.addFirst(5);
-        arr.addLast(0);
-        arr.addLast(-1);
-        arr.addLast(-2);
-        arr.addLast(-3);
-        System.out.println(arr.get(2));
-        arr.printDeque();
-        arr.removeFirst();
-        arr.removeFirst();
-        arr.removeFirst();
-        arr.removeFirst();
-        arr.removeLast();
-        arr.removeLast();
-        arr.printDeque();
-        System.out.println(arr.get(1));
-    }
 }
